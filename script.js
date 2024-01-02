f4dac4ab30230a8c2447d1175c4808f9f845b31c
mukeshcs2018 <mukesh.cs2018@gmail.com>
2024-01-02 12:35:01 +0530

    Add CSS in Buttons

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hello");
+"use strict";
+
+async function fetchSong() {
+  let url = await fetch("http://127.0.0.1:3000/songs/");
+  let response = await url.text();
+  let div = document.createElement("div");
+  div = response;
+  console.log(response);
+}
+
+fetchSong();

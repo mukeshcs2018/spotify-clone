3ec06758809ddfa022f826766c64b044b93fbab5
mukeshcs2018 <mukesh.cs2018@gmail.com>
2024-01-04 11:23:41 +0530

    add favicon

--- script.js
+++ script.js
@@ -4,8 +4,42 @@ async function fetchSong() {
   let url = await fetch("http://127.0.0.1:3000/songs/");
   let response = await url.text();
   let div = document.createElement("div");
-  div = response;
-  console.log(response);
+  div.innerHTML = response;
+
+  let as = div.getElementsByTagName("a");
+
+  let songs = [];
+
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  return songs;
 }
 
-fetchSong();
+async function main() {
+  let songs = await fetchSong();
+
+  let songUL = document
+    .querySelector(".songs-list")
+    .getElementsByTagName("ul")[0];
+
+  for (const song of songs) {
+    songUL.innerHTML += `<li>
+    <img class="invert" src="./svgs/music.svg" alt="" />
+    <div class="info">
+      <div>${song.replaceAll("%20", " ")}</div>
+      <div>Nigga</div>
+    </div>
+    <div class="play-now">
+      <span>Play Now</span>
+      <img class="invert" src="./svgs/play.svg" alt="" />
+    </div>
+  </li>`;
+  }
+}
+
+main();

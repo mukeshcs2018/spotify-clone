c87c8721eb3c2fd893bc6e8d5ab52b6a60b85b7c
mukeshcs2018 <mukesh.cs2018@gmail.com>
2024-01-16 01:29:53 +0530

    Fixed Some major Bugs, Added seekbar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let currentSongIndex =  0
 
 
 async function fetchSongs(){
-  let response = await fetch("http://127.0.0.1:3000/songs/");
+  let response = await fetch("./songs/");
   response = await response.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -42,49 +42,96 @@ function displaySongsInLib(songs){
   }
 }
 
+// for changing seekbar
+ function onChangeInput(){
+  let seekbar = document.querySelector(".seekbar")
+  audio.currentTime = seekbar.value
+ }
+
+
 
 function playMusic(currentSongIndex, songsDir){
-  let currentSongURI = songsDir[currentSongIndex];
 
-  let currentSongName = decodeURI(currentSongURI).split("/songs/")[1].split('-')[0];
+  let currentSongName = decodeURI(songsDir[currentSongIndex]).split("./songs/")[1].split('-')[0];
 
   document.getElementById('song-name').innerHTML = currentSongName;
+  audio.src = songsDir[currentSongIndex];
 
-  console.log(currentSongName)
-  audio.src = songsDir[currentSongIndex]
   audio.play();
+
+  audio.addEventListener("timeupdate", ()=>{
+
+    document.querySelector(".song-duration").innerHTML = `${songTimer(audio.currentTime)}/${!isNaN(audio.duration) ? songTimer(audio.duration): '00:00'}`
+    
+    
+    
+    let seekbar = document.querySelector(".seekbar")
+    seekbar.max = audio.duration
+    seekbar.value = audio.currentTime  
+
+  })
+
+
+
+
+  function songTimer(durationInSeconds){
+
+    const minutes = Math.floor(durationInSeconds / 60);
+    const seconds = Math.floor(durationInSeconds % 60);
+
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  }
+  
 }
+
 
 
 async function main(){
   let songsDir = await fetchSongs();
   displaySongsInLib(songsDir);
-
-
+ 
   Array.from(document.querySelector(".songs-list").getElementsByTagName("li")).forEach((ele, index) =>{
         ele.addEventListener("click", () => {
             currentSongIndex = index
-            playMusic(currentSongIndex, songsDir);          
+            playMusic(currentSongIndex, songsDir);   
+            play.src = "./svgs/paused.svg"       
         })
   })
 
-  play.addEventListener("click",function(){
-    if(!audio.paused){
-      audio.pause()
-      play.src = "/svgs/play-btn.svg"
-    }
-    else{
-      audio.play();
-      playMusic(currentSongIndex, songsDir);
-      play.src = "/svgs/paused.svg"
-    }
-  })
 
+  function pauseAudio() {
+    audio.pause();
+    play.src = "./svgs/play-btn.svg";
+  }
+
+  function resumeAudio() {
+    // Save the current playback position
+    var currentPlaybackTime = audio.currentTime;
+  
+    playMusic(currentSongIndex, songsDir);
+  
+    // Set the saved playback position
+    audio.currentTime = currentPlaybackTime;
+    play.src = "./svgs/paused.svg";
+  }
+  
+
+  play.addEventListener("click", function () {
+    if (audio.paused) {
+      resumeAudio();
+    } else {
+      pauseAudio();
+    }
+  });
+  
+
+
+//  currentSongIndex = 1
+    
   prev.addEventListener("click", ()=>{
-    currentSongIndex -= 1;
 
     if(currentSongIndex >= 0){
-      playMusic(currentSongIndex, songsDir);
+      playMusic(currentSongIndex-=1, songsDir);
     }
     else{
       playMusic(0, songsDir);
@@ -92,14 +139,15 @@ async function main(){
   })
 
   next.addEventListener("click",()=>{
-    currentSongIndex += 1;
-    if(currentSongIndex < songsDir.length){
-      playMusic(currentSongIndex, songsDir)
+
+    if(currentSongIndex < songsDir.length-1){
+      playMusic(currentSongIndex += 1, songsDir)
     }
     else{
         playMusic(songsDir.length - 1, songsDir)
     }
   })
+
 
 }   
 
